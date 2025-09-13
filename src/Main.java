625b6c5f075914f40b7e1d7823d73e76d3ebda78
CollegeWork-ship <wood2ej@mail.uc.edu>
2025-09-13 11:17:03 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    private static Object numToExamine;
+
+    public static void main(String[] args) {
+    int numToExamine = 2;
+        if (numToExamine % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+
+    private static Object assertThat(int i) {
+        return null;
+    }
+}
